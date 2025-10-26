f2e61959f5c7c8c7c244e15e7571ff474259b441
Heschung Erwan <erwan.heschung@etu.univ-cotedazur.fr>
2025-10-26 21:44:44 +0100

    feat: add dockerized version

--- micro-services/Auth/src/main/resources/init.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE role
-    ALTER COLUMN id SET DEFAULT gen_random_uuid();
-INSERT INTO role (name, description)
-VALUES
-    ('DOCTOR', 'Role for medical doctors'),
-    ('RELATIVE', 'Role for relatives of patients');
